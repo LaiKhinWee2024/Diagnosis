081bd5ce7c8ac7e03dc971e974ab9f567ca751cf
LaiKhinWee2024 <160090039+LaiKhinWee2024@users.noreply.github.com>
2024-03-07 20:27:04 +0800

    Update memorybot.py

--- memorybot.py
+++ memorybot.py
@@ -8,6 +8,8 @@ from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationEntityMemory
 from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
 from langchain.llms import OpenAI
+from pydantic.error_wrappers import ValidationError
+
 
 # initialise session states
 
